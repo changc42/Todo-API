4e9297bfd6f898996cd4f046b930503868b77c93
Caleb Chang <changc42@gmail.com>
2020-01-16 13:31:43 -0500

    u

--- server.js
+++ server.js
@@ -2,8 +2,22 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3500;
 
+var todos = [{
+    id: 1,
+    description: "walk the fish",
+    completed: false,
+},{
+    id:3,
+    description: "world domination",
+    completed: false,  
+}]
+
 app.get("/",(req,res)=>{
     res.send("todo API root")
+})
+
+app.get("/todos",(req,res)=>{
+    res.json(todos)
 })
 
 app.listen(PORT,()=>{
